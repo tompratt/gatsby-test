5cb6ee38c590322ed251df606a1d1a090c7324e5
Tom Pratt <tom@thisiscatalogue.co.uk>
2021-09-02 16:33:30 +0100

    Finished part 3 tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gatsby Site Test",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
